486e3911cee13eada5f353d813a3722883712ca7
nathalex nathalex <nathalex@f1br10s7.paris.42.school>
2026-07-24 00:56:50 +0200

    push

---
+++ Compilation and Preprocessing/C-Preprocessor-07c9c9b7-75c9-43af-bf6d-8b5e9e259b70/ex0/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nathalex <nathalex@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/23 23:09:26 by nathalex          #+#    #+#             */
+/*   Updated: 2026/07/24 00:26:11 by nathalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ Compilation and Preprocessing/C-Preprocessor-07c9c9b7-75c9-43af-bf6d-8b5e9e259b70/ex1/ft_boolean.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nathalex <nathalex@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/24 00:24:53 by nathalex          #+#    #+#             */
+/*   Updated: 2026/07/24 00:25:28 by nathalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define SUCCESS 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef int	t_bool;
+void	ft_putstr(char *str);
+t_bool	ft_is_even(int nbr);
+
+#endif
